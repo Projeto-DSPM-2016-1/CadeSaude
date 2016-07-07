b205a87b052cdf862265b570ac4bf55a1c22318d
Thiago Nóbrega <thiago_nrodrigues@yahoo.com.br>
2016-07-06 21:43:50 -0300

    Criando objeto posto com os dados JSON do servidor

--- app/src/main/java/dspm/dc/ufc/br/cadesaude/Statics.java
+++ app/src/main/java/dspm/dc/ufc/br/cadesaude/Statics.java
@@ -4,5 +4,5 @@ package dspm.dc.ufc.br.cadesaude;
  * Created by Thiago on 06/07/2016.
  */
 public class Statics {
-    public final static String GET_POSTOS_SERVER_URL = "http://cadesaude.hostingsiteforfree.com/getpostosjson.php?from=1&to=50";
+    public final static String GET_POSTOS_SERVER_URL = "http://cadesaude.hostingsiteforfree.com/getpostosjson.php?from=1&to=10";
 }
